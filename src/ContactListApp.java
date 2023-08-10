95d2d38a7159e0e6c7db01a2c84076ae322dc010
Jeremy Tanner <tanner.jeremy.j@gmail.com>
2023-08-10 15:37:49 -0500

    updated addnewcontact method

--- src/ContactListApp.java
+++ src/ContactListApp.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ContactListApp {
     public static Input input = new Input();
-
+    private static Path contactFilePath = Paths.get("src/contacts.txt");
     public static void main(String[] args) {
         trigger();
     }
@@ -29,7 +31,7 @@ public class ContactListApp {
     public static void trigger() {
         ArrayList list = createArray();
 //        Input input = new Input();
-        Path contactFilePath = Paths.get("src/contacts.txt");
+//        Path contactFilePath = Paths.get("src/contacts.txt");
         System.out.println(createArray());
         //display menu here
         displayMenu();
@@ -47,6 +49,7 @@ public class ContactListApp {
 
 
     }
+
     public static ArrayList createArray() {
         ArrayList<Contact> contacts = new ArrayList<>();
         try {
@@ -61,6 +64,7 @@ public class ContactListApp {
         }
         return contacts;
     }
+
     public static void viewContacts() {
         try {
             List<String> lineOfTexts = Files.readAllLines(
@@ -77,14 +81,42 @@ public class ContactListApp {
     }
 
     public static void addNewContact() {
-        System.out.println("Enter new contact name: ");
-        String contactName = input.getString();
+        try {
+            System.out.println("Enter new contact name: ");
+            String contactName = input.getString();
+            String newContactPhoneNumber = validatePhoneLength();
+            String newContact = contactName + " " + newContactPhoneNumber;
+            Files.write(contactFilePath, Collections.singletonList(newContact), StandardOpenOption.APPEND);
+//            Files.write(contactFilePath, Collections.singletonList(newContactPhoneNumber), StandardOpenOption.APPEND);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static String validatePhoneLength() {
         System.out.println("Enter new contact phone number: ");
         int contactNumber = input.getInt();
         String contactPhone = String.valueOf(contactNumber);
-        String firstThree = contactPhone.substring(0,3);
-        System.out.println(firstThree);
-    }
+        String formattedPhone = "";
+        if (contactPhone.length() == 10) {
+            String firstThree = contactPhone.substring(0, 3);
+            String secondThree = contactPhone.substring(3, 6);
+            String lastFour = contactPhone.substring(6, 10);
+            formattedPhone = "(" + firstThree + ") " + secondThree + "-" + lastFour;
+            return formattedPhone;
+        } else if (contactPhone.length() == 7) {
+            String firstThree = contactPhone.substring(0, 3);
+            String lastFour = contactPhone.substring(3, 7);
+            formattedPhone = firstThree + "-" + lastFour;
+            return formattedPhone;
 
+        } else {
+            System.out.println("Phone number has incorrect number of digits.");
+            return validatePhoneLength();
+        }
+
+    }
 //ending curly brace
 }
